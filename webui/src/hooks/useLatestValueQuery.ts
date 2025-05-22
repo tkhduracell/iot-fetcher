63e806ea3348fe18511bbb8faf2e6071988b7aa2
Filip Lindqvist <buggfille@gmail.com>
2025-05-22 10:37:34 +0200

    My changes committed for PR (#14)

---
+++ webui/src/hooks/useLatestValueQuery.ts
@@ -0,0 +1,33 @@
+import { useMemo } from 'react';
+import useFluxQuery from './useFluxQuery';
+
+export interface LatestValueQueryParams {
+  bucket?: string;
+  measurement: string;
+  field: string;
+  filter?: {[key: string]: string};
+  window?: string;
+  range?: string;
+}
+
+function useLatestValueQuery({ bucket = "irisgatan", measurement, field, filter = {}, window = "5m", range = "-15m" }: LatestValueQueryParams) {
+  const fullFilter = useMemo(() => ({
+    '_measurement': measurement,
+    '_field': field,
+    ...filter
+  }), [measurement, field, filter]);
+
+  const filterQuery = useMemo(() => Object.entries(fullFilter)
+    .map(([k,v]) => `|> filter(fn: (r) => r["${k}"] == "${v}") `)
+    .join('\n    '), [fullFilter]);
+
+  const fluxQuery = useMemo(() => `from(bucket: "${bucket}")
+    |> range(start: ${range})
+    ${filterQuery}
+    |> aggregateWindow(every: ${window}, fn: last, createEmpty: false)
+    |> yield(name: "last")`, [bucket, range, filterQuery, window]);
+
+  return useFluxQuery({ fluxQuery: fluxQuery.toString() });
+}
+
+export default useLatestValueQuery;
