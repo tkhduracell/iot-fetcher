f49e24f618d2cf98d72668c5fb15a8685b98c440
Filip Lindqvist <buggfille@gmail.com>
2025-08-18 12:27:01 +0200

    feat: implement InfluxDB proxy with upload functionality and enhance web UI

--- balena-envs.sh
+++ balena-envs.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <fleet_name> <device>"
+    echo "Usage: $0 <fleet_name>"
     exit 1
 fi
 
 FLEET_NAME="$1"
-DEVICE="$2"
+
+while IFS= read -r line || [ -n "$line" ]; do
+    if [ -n "$line" ]; then
+        IFS='=' read -r var value <<< "$line"
+        # if it starts with a comment or is empty, skip it
+        if [[ "$var" == \#* ]] || [ -z "$var" ]; then
+            continue
+        fi
+        echo "balena env set --fleet "$FLEET_NAME" --service influx-proxy "$var" <...>"
+        balena env set --fleet "$FLEET_NAME" --service influx-proxy "$var" "$value"
+    fi
+done < "influx-proxy/.env"
 
 while IFS= read -r line || [ -n "$line" ]; do
     if [ -n "$line" ]; then
--- start.sh
+++ start.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 # if arguments are passed, use them as parameters for the main script
-if [ $# -gt 0 ]; then
+# if the first argument if -- remove it
+
+if [ "$1" = "--" ]; then
+    shift
+fi
+
+# If argument is set
+if [ -n "$1" ]; then
     echo "Arguments passed to the script: $@"
-    python3 ./python/src/main.py $@
+
+    # the arg is webui
+    if [ "$1" = "webui" ]; then
+        python3 ./webui/web.py "$@"
+        exit 0
+    fi
+
+    python3 ./python/src/main.py "$@"
     exit 0
 fi
 
@@ -11,7 +25,7 @@ python3 ./webui/web.py &
 
 python3 ./python/src/main.py &
 
-npm --prefix nodejs run start &
+node ./nodejs/dist/index.js &
 
 # Wait for all background processes to finish
 wait
