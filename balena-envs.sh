0048ba3802828be260953464fff54824ad2ca49d
Filip Lindqvist <buggfille@gmail.com>
2026-02-08 19:04:29 +0100

    Refactor Docker setup to use VictoriaMetrics (#169)
    * refactor: replace influxdb3 service with victoria-metrics in docker-compose.local.yml
    
    * chore: Move fetcher parts to own folder
    
    * chore: Split up application parts
    
    * chore: Update Dependabot configuration and adjust Docker build context in workflows
    
    * chore: Add start.sh script to manage background processes for webui and main application

--- balena-envs.sh
+++ balena-envs.sh
@@ -17,7 +17,7 @@ while IFS= read -r line || [ -n "$line" ]; do
         echo "balena env set --fleet "$FLEET_NAME" --service iot-fetcher $var <...>"
         balena env set --fleet "$FLEET_NAME" --service iot-fetcher $var ${value//[\`\'\"]/}
     fi
-done < ".env"
+done < "fetcher-core/.env"
 
 while IFS= read -r line || [ -n "$line" ]; do
     if [ -n "$line" ]; then
@@ -42,42 +42,6 @@ while IFS= read -r line || [ -n "$line" ]; do
         balena env set --fleet "$FLEET_NAME" --service garmin-influx-bridge $var ${value//[\`\'\"]/}
     fi
 done < "garmin-influx-bridge/.env"
-
-while IFS= read -r line || [ -n "$line" ]; do
-    if [ -n "$line" ]; then
-        IFS='=' read -r var value <<< "$line"
-        # if it starts with a comment or is empty, skip it
-        if [[ "$var" == \#* ]] || [ -z "$var" ]; then
-            continue
-        fi
-        echo "balena env set --fleet "$FLEET_NAME" --service database $var <...>"
-        balena env set --fleet "$FLEET_NAME" --service database $var ${value//[\`\'\"]/}
-    fi
-done < "influx-2/.env"
-
-while IFS= read -r line || [ -n "$line" ]; do
-    if [ -n "$line" ]; then
-        IFS='=' read -r var value <<< "$line"
-        # if it starts with a comment or is empty, skip it
-        if [[ "$var" == \#* ]] || [ -z "$var" ]; then
-            continue
-        fi
-        echo "balena env set --fleet "$FLEET_NAME" --service influx-3 $var <...>"
-        balena env set --fleet "$FLEET_NAME" --service influx-3 $var ${value//[\`\'\"]/}
-    fi
-done < "influx-3/.env"
-
-while IFS= read -r line || [ -n "$line" ]; do
-    if [ -n "$line" ]; then
-        IFS='=' read -r var value <<< "$line"
-        # if it starts with a comment or is empty, skip it
-        if [[ "$var" == \#* ]] || [ -z "$var" ]; then
-            continue
-        fi
-        echo "balena env set --fleet "$FLEET_NAME" --service iot-fetcher $var <...>"
-        balena env set --fleet "$FLEET_NAME" --service iot-fetcher $var ${value//[\`\'\"]/}
-    fi
-done < ".env"
 
 while IFS= read -r line || [ -n "$line" ]; do
     if [ -n "$line" ]; then
